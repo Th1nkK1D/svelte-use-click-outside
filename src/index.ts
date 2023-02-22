3252b6821950395be07044406942ee415eeb5e6a
Cory Tyburski <corytyburski@gmail.com>
2023-02-22 00:45:54 -0500

    Fix false positives when the user clicks up outside the element

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,33 @@
-export function clickOutside(
-  node: HTMLElement,
-  handler: () => void
-): { destroy: () => void } {
-  const onClick = (event: MouseEvent) =>
-    node &&
-    !node.contains(event.target as HTMLElement) &&
-    !event.defaultPrevented &&
-    handler();
+export function clickOutside(node: HTMLElement, handler: () => void): { destroy: () => void } {
+	let pointerDownStartedInside = false;
 
-  document.addEventListener('click', onClick, true);
+	const onPointerDown = (event: PointerEvent) => {
+		if (node && node.contains(event.target as HTMLElement) && !event.defaultPrevented) {
+			// Set our flag to True if the pointer started from within the node.
+			// This is needed to prevent regular `pointerup` event from triggering the handler
+			pointerDownStartedInside = true;
+		}
+	};
 
-  return {
-    destroy() {
-      document.removeEventListener('click', onClick, true);
-    },
-  };
+	const onPointerUp = (event: PointerEvent) => {
+		if (node && !node.contains(event.target as HTMLElement) && !event.defaultPrevented) {
+			if (pointerDownStartedInside) {
+			} else {
+				handler();
+			}
+		}
+
+		// Reset the flag
+		pointerDownStartedInside = false;
+	};
+
+	document.addEventListener('pointerup', onPointerUp, true);
+	document.addEventListener('pointerdown', onPointerDown, true);
+
+	return {
+		destroy() {
+			document.removeEventListener('pointerup', onPointerUp, true);
+			document.removeEventListener('pointerdown', onPointerDown, true);
+		}
+	};
 }
