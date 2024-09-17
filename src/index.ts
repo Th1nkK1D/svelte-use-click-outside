0f5b6c202160835a6073f847626503a4f8257309
Th1nkK1D <witheep@gmail.com>
2024-09-17 20:48:55 +0700

    refactor: improve condition statement and format code

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,38 @@
-export function clickOutside(node: HTMLElement, handler: () => void): { destroy: () => void } {
-	let pointerDownStartedInside = false;
+export function clickOutside(
+  node: HTMLElement,
+  handler: () => void
+): { destroy: () => void } {
+  let pointerDownStartedInside = false;
 
-	const onPointerDown = (event: PointerEvent) => {
-		if (node && node.contains(event.target as HTMLElement) && !event.defaultPrevented) {
-			// Set our flag to True if the pointer started from within the node.
-			// This is needed to prevent regular `pointerup` event from triggering the handler
-			pointerDownStartedInside = true;
-		}
-	};
+  const onPointerDown = (event: PointerEvent) => {
+    if (
+      !event.defaultPrevented &&
+      node?.contains(event.target as HTMLElement)
+    ) {
+      pointerDownStartedInside = true;
+    }
+  };
 
-	const onPointerUp = (event: PointerEvent) => {
-		if (node && !node.contains(event.target as HTMLElement) && !event.defaultPrevented) {
-			if (pointerDownStartedInside) {
-			} else {
-				handler();
-			}
-		}
+  const onPointerUp = (event: PointerEvent) => {
+    if (
+      !event.defaultPrevented &&
+      !pointerDownStartedInside &&
+      node &&
+      !node.contains(event.target as HTMLElement)
+    ) {
+      handler();
+    }
 
-		// Reset the flag
-		pointerDownStartedInside = false;
-	};
+    pointerDownStartedInside = false;
+  };
 
-	document.addEventListener('pointerup', onPointerUp, true);
-	document.addEventListener('pointerdown', onPointerDown, true);
+  document.addEventListener('pointerup', onPointerUp, true);
+  document.addEventListener('pointerdown', onPointerDown, true);
 
-	return {
-		destroy() {
-			document.removeEventListener('pointerup', onPointerUp, true);
-			document.removeEventListener('pointerdown', onPointerDown, true);
-		}
-	};
+  return {
+    destroy() {
+      document.removeEventListener('pointerup', onPointerUp, true);
+      document.removeEventListener('pointerdown', onPointerDown, true);
+    },
+  };
 }
